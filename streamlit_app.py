47876940732db740300f0cbbddbfeb86b5b1a494
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-02-29 12:41:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,22 @@
 import streamlit as st
 import streamlit as st
 import google.generativeai as genai
-from IPython.display import Markdown
 import textwrap
 
 genai.configure(api_key="AIzaSyDlBFVsmV8pao6Ax-bcR0dc5h4CusiNCsc")
 model = genai.GenerativeModel(model_name="gemini-pro")
-
-def to_markdown(text):
-    text = text.replace('•', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
 def generate_blog_outline(topic):
     human_prompt = f"'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'"
     response = model.generate_content(human_prompt)
     return response.text
 
+# Define Streamlit UI
 st.title("Generate Blog Outline")
 topic = st.text_input("Enter the topic you want to generate a blog outline about:")
 if st.button("Generate Outline"):
     if topic:
         outline = generate_blog_outline(topic)
-        st.markdown(to_markdown(outline))
+        st.markdown(outline)
     else:
         st.warning("Please enter a topic.")
 
